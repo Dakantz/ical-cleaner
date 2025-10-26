235027e0206ea0420f256f0b44023e65fd3f2e51
Benedikt Kantz <benedikt@kantz.at>
2025-10-26 17:20:33 +0100

    add combine

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import requests
 import pymongo
 from bson import ObjectId
 from icalendar import Calendar, Event
+
 # Replace the uri string with your MongoDB deployment's connection string.
 conn_str = getenv("MONGODB_CONN", "mongodb://root:devpassword@localhost:27017/")
 
@@ -12,8 +13,7 @@ client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 try:
     print(client.server_info())
 except Exception:
-    
-    print("Unable to connect to the server.",conn_str)
+    print("Unable to connect to the server.", conn_str)
     exit(-1)
 
 mdb = client.get_database("ical-wrapper")
@@ -27,48 +27,61 @@ def calendar(id):
     print("Loading calendar for", id)
     calendar_options = calendars.find_one(ObjectId(id))
     if calendar_options:
-        print("found options...")
-        block_words = calendar_options['blocked_words']
-        web_ical_data = requests.get(calendar_options['url'])
-
         cal = Calendar()
-        web_ical: Calendar = cal.from_ical(web_ical_data.text)
         transformed_ical = Calendar()
+        print("found options...")
+        block_words = calendar_options["blocked_words"]
+        urls_to_combine: dict[str, str] = calendar_options.get("urls_combine", {})
+        for prefix, url in urls_to_combine.items():
+            print("combining from", url)
+            web_ical_data = requests.get(url)
+            cal = Calendar()
+            web_ical: Calendar = cal.from_ical(web_ical_data.text)
+            for evt in web_ical.subcomponents:
+                if evt.name != "VEVENT":
+                    continue
+                evt_prefix = prefix + " " if prefix else ""
+                
+                evt["SUMMARY"] = f"{evt_prefix} {evt['SUMMARY'] if 'SUMMARY' in evt else ''}".strip()
+                transformed_ical.add_component(evt)
+
+        web_ical_data = requests.get(calendar_options["url"])
+        web_ical: Calendar = cal.from_ical(web_ical_data.text)
+
         for evt in web_ical.subcomponents:
             is_good = True
             for word in block_words:
-                if word in str(evt['SUMMARY']):
+                if word in str(evt["SUMMARY"]):
                     is_good = False
                     break
             if is_good:
                 transformed_ical.add_component(evt)
         resp = make_response(transformed_ical.to_ical(), 200)
-        resp.headers['content-type'] = "text/calendar;charset=UTF-8"
-        resp.headers['content-disposition'] = "attachment; filename=calendar.ics"
+        resp.headers["content-type"] = "text/calendar;charset=UTF-8"
+        resp.headers["content-disposition"] = "attachment; filename=calendar.ics"
         return resp
     else:
         return make_response("nope", 404)
 
 
-@app.route("/calendar-options", methods=['GET', 'POST'])
+@app.route("/calendar-options", methods=["GET", "POST"])
 def calendar_options():
     print("got new cal req")
     id = None
-    if 'id' in request.args:
-        id = request.args['id']
+    if "id" in request.args:
+        id = request.args["id"]
     data = None
     if request.method == "POST" and request.is_json:
         if id:
-            data = calendars.find_one_and_replace({
-                '_id': id
-            }, request.json)
+            data = calendars.find_one_and_replace({"_id": ObjectId(id)}, request.json)
         else:
             inserted = calendars.insert_one(request.json)
-            request.json['id'] = str(inserted.inserted_id)
+            request.json["id"] = str(inserted.inserted_id)
             data = request.json
-            del data['_id']
+            del data["_id"]
+            return data
     else:
-        data = calendars.find_one({
-            '_id': id
-        })
+        data = calendars.find_one({"_id": ObjectId(id)})
+    
+    del data["_id"]
     return data
